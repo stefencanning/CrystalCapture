f32d1d6e6d53441957c6c6e48f5bd23e47cecc8d
stefen <stefencanning@hotmail.com>
2014-12-02 17:34:49 +0000

    changes

--- js/Images.js
+++ js/Images.js
@@ -49,7 +49,7 @@ Images.prototype.BodyImages = function()
 				{
 					strCol="black";
 				}
-				images.bodies[i][j][k].src="img/"+strGen+strRace+strCol+".png";
+				images.bodies[i][j][k].src="img/body/"+strGen+strRace+strCol+".png";
 			}
 			strRace="elf - "; 
 		}
@@ -62,24 +62,24 @@ Images.prototype.CrystalImages = function()
 	images.redCaptureImg = [new Image(),new Image(),new Image()];
 	images.blueCaptureImg = [new Image(),new Image(),new Image()];
 	images.redStandingCrystal=new Image();
-	images.redStandingCrystal.src = 'img/redCrystal.png';
+	images.redStandingCrystal.src = 'img/crystal/redCrystal.png';
 	images.blueStandingCrystal=new Image();
-	images.blueStandingCrystal.src = 'img/blueCrystal.png';
+	images.blueStandingCrystal.src = 'img/crystal/blueCrystal.png';
 	images.redCrystalBase=new Image();
-	images.redCrystalBase.src = 'img/redCrystalBase.png';
+	images.redCrystalBase.src = 'img/crystal/redCrystalBase.png';
 	images.blueCrystalBase=new Image();
-	images.blueCrystalBase.src = 'img/blueCrystalBase.png';
+	images.blueCrystalBase.src = 'img/crystal/blueCrystalBase.png';
 	images.redGrabbedCrystal=new Image();
-	images.redGrabbedCrystal.src = 'img/redGrabbedCrystal.png';
+	images.redGrabbedCrystal.src = 'img/crystal/redGrabbedCrystal.png';
 	images.blueGrabbedCrystal=new Image();
-	images.blueGrabbedCrystal.src = 'img/blueGrabbedCrystal.png';
+	images.blueGrabbedCrystal.src = 'img/crystal/blueGrabbedCrystal.png';
 	
 	
-	images.redCaptureImg[0].src = 'img/redCaptureBot.png';
-	images.redCaptureImg[1].src = 'img/redCaptureMid.png';
-	images.redCaptureImg[2].src = 'img/redCaptureTop.png';
+	images.redCaptureImg[0].src = 'img/crystal/redCaptureBot.png';
+	images.redCaptureImg[1].src = 'img/crystal/redCaptureMid.png';
+	images.redCaptureImg[2].src = 'img/crystal/redCaptureTop.png';
 	
-	images.blueCaptureImg[0].src = 'img/blueCaptureBot.png';
-	images.blueCaptureImg[1].src = 'img/blueCaptureMid.png';
-	images.blueCaptureImg[2].src = 'img/blueCaptureTop.png';
+	images.blueCaptureImg[0].src = 'img/crystal/blueCaptureBot.png';
+	images.blueCaptureImg[1].src = 'img/crystal/blueCaptureMid.png';
+	images.blueCaptureImg[2].src = 'img/crystal/blueCaptureTop.png';
 }
